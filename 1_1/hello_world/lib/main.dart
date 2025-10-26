593c1193c670e9e9acd43004e590305be0c58ca5
Samarok <ars2004sam@gmail.com>
2025-10-26 18:54:45 +0300

    1_1 hello world p5

--- 1_1/hello_world/lib/main.dart
+++ 1_1/hello_world/lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData(primarySwatch: Colors.blue),
-            home: Scaffold(
-                appBar: AppBar(title: Text('This is Flutter')),
-                body: Center(child: Text('Hello World'))
-            )
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.red, // Добавляем красный фон
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: Center(
+          child: Text(
+            'Привет, Мир!',
+            style: TextStyle(fontSize: 32, color: Colors.yellow), // Увеличиваем текст и делаем его желтым
+          ),
+        ),
+      ),
+    );
+  }
 }
